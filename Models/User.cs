107aa899ada6e076b8db3919b66df92b6c268094
dovgalev92 <dovgalev2021@inbox.ru>
2022-12-11 23:36:41 +0300

    Добавьте файлы проекта.

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Roles.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        [Compare("Password", ErrorMessage = "Обнаружена несоответствие пароля")]
+        public string? RepeatPassword { get; set; }
+        public Roles? Roles { get; set; }
+    }
+}
