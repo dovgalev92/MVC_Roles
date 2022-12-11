bf58cd7e389887a6f0a587b6016a49e7ed67a943
dovgalev92 <dovgalev2021@inbox.ru>
2022-12-12 02:16:27 +0300

    Добавил метод RegistrUser. Добавил Roles

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -1,13 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Roles.Context;
+using MVC_Roles.Models;
+using System.Security.Claims;
 
 namespace MVC_Roles.Controllers
 {
+    [Route("{controller}")]
     public class AccessController : Controller
     {
-        [Route("/Start")]
+        private readonly Context_Data context;
+        public AccessController(Context_Data context)
+        {
+            this.context = context;
+        }
+        [Route("LoginIn")]
         public IActionResult Index()
         {
             return View();
         }
+        [Route("Registr")]
+        public async Task<IActionResult> RegisterUser(RegistModel regist)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = context.Users.FirstOrDefault(u => u.Email.Equals(regist.Email));
+                if(user == null)
+                {
+                    user = new User { Email = regist.Email, Password = regist.Password };
+                    Roles roles = await context.Roles.FirstOrDefaultAsync(n => n.Name == "Пользователь");
+                    if(roles!= null)
+                    {
+                        user.Roles = roles;
+                    }
+                    context.Users.Add(user);
+                    await context.SaveChangesAsync();
+                    await Authorisation(user);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(regist);
+        }
+
+
+
+        public async Task Authorisation(User user)
+        {
+            var claim = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Roles?.Name)
+            };
+            var claimIdentity = new ClaimsIdentity(claim, "Cookies", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(claimIdentity));
+        }
     }
+    
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace MVC_Roles.Controllers
 {
+    [Route("{controller}")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -12,16 +13,12 @@ namespace MVC_Roles.Controllers
         {
             _logger = logger;
         }
-
+        [Route("Index")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
